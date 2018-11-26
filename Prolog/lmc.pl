dcf74f7c4ab04d2f41895176e55f12678dbef130
fdila <federicadilauro1998@gmail.com>
2018-11-26 12:16:25 +0100

    Aggiunte branch, branch if zero, branch positive, input, output, halt.

--- Prolog/lmc.pl
+++ Prolog/lmc.pl
@@ -43,12 +43,76 @@ one_instruction(state(Acc, PC, Mem, In, Out, Flag),
 									!.
 
 %Load
-one_instruction(state(Acc, PC, Mem, In, Out, Flag),
+one_instruction(state(_, PC, Mem, In, Out, Flag),
 								state(Acc2, PC2, Mem, In, Out, Flag)) :-
 									nth0(PC, Mem, Inst, _),
 									Inst > 499,
 									Inst < 600,
 									Pos is Inst - 500,
 									nth0(Pos, Mem, Acc2, _),
-									PC2 is PC + 1,
+									PC2 is mod(PC + 1, 100),
+									!.
+
+%Branch
+one_instruction(state(Acc, PC, Mem, In, Out, Flag),
+								state(Acc, PC2, Mem, In, Out, Flag)) :-
+									nth0(PC, Mem, Inst, _),
+									Inst > 599,
+									Inst < 700,
+									PC2 is Inst - 600.
+
+%Branch if zero
+one_instruction(state(Acc, PC, Mem, In, Out, Flag),
+								state(Acc, PC2, Mem, In, Out, Flag)) :-
+									nth0(PC, Mem, Inst, _),
+									Inst > 699,
+									Inst < 800,
+									PCBranch is Inst - 700,
+									lmc_branch_zero(Acc, PC, PCBranch, Flag, PC2),
+									!.
+
+lmc_branch_zero(0, _, PCBranch, noflag, PCBranch) :- !.
+lmc_branch_zero(_, PC, _, _, PC2) :-
+									PC2 is mod(PC + 1, 100),
+									!.
+
+%Branch if positive
+one_instruction(state(Acc, PC, Mem, In, Out, Flag),
+								state(Acc, PC2, Mem, In, Out, Flag)) :-
+									nth0(PC, Mem, Inst, _),
+									Inst > 799,
+									Inst < 900,
+									PCBranch is Inst - 800,
+									lmc_branch_positive(PC, PCBranch, Flag, PC2),
+									!.
+
+lmc_branch_positive(_, PCBranch, noflag, PCBranch) :- !.
+lmc_branch_positive(PC, _, flag, PC2) :-
+											PC2 is mod(PC + 1, 100),
+											!.
+
+%Input
+one_instruction(state(_, PC, Mem, In, Out, Flag),
+								state(Acc2, PC2, Mem, In2, Out, Flag)) :-
+									nth0(PC, Mem, Inst, _),
+									Inst = 901,
+									nth0(0, In, Acc2, In2),
+									PC2 is mod(PC + 1, 100),
+									!.
+%Output
+one_instruction(state(Acc, PC, Mem, In, Out, Flag),
+								state(Acc, PC2, Mem, In, Out2, Flag)) :-
+									nth0(PC, Mem, Inst, _),
+									Inst = 902,
+									append(Out, [Acc], Out2),
+									PC2 is mod(PC + 1, 100),
+									!.
+
+%Halt
+%TODO come fermare esecuzione programma?
+one_instruction(state(Acc, PC, Mem, In, Out, Flag),
+								halted_state(Acc, PC2, Mem, In, Out, Flag)) :-
+									nth0(PC, Mem, Inst, _),
+									Inst < 100,
+									PC2 is mod(PC + 1, 100),
 									!.
