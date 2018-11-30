e43cc4d9c0b462ba8860fbec95f06b5a32e2f339
fdila <federicadilauro1998@gmail.com>
2018-11-30 13:43:04 +0100

    Prova per la lettura del file.

---
+++ Prolog/read.pl
@@ -0,0 +1,10 @@
+:- use_module(library(unicode)).
+:- use_module(library(readutil)).
+
+/*
+Così leggo il file, e lo trasformo lowercase
+
+read_file_to_codes("/home/fede/Documents/Progetti/little-man-computer/Prolog/multiplication.lmc", X, []),
+string_codes(Y, X),
+string_lower(Y, Z).
+*/
