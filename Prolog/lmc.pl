4d1f4895493735ceb7d4f7ec01157569cc93309e
fdila <federicadilauro1998@gmail.com>
2018-12-03 11:01:53 +0100

    Spostata parte di parsing, aggiunti lmc_load e lmc_run

--- Prolog/lmc.pl
+++ Prolog/lmc.pl
@@ -1,13 +1,5 @@
 %% -*- Mode: Prolog -*-
-
-% necessario per evitare che prolog mi metta i puntini su cose troppo lunghe
-:- set_prolog_flag(answer_write_options,
-                   [ quoted(true),
-                     portray(true),
-                     spacing(next_argument)
-                   ]).
-
-
+:- use_module(library(readutil)).
 % Addizione
 one_instruction(state(Acc, PC, Mem, In, Out, _),
 								state(Acc2, PC2, Mem, In, Out, Flag)) :-
@@ -130,3 +122,189 @@ execution_loop(state(Acc, PC, Mem, In, Out, Flag), Out2) :-
 								one_instruction(state(Acc, PC, Mem, In, Out, Flag),
 																NewState),
 								execution_loop(NewState, Out2), !.
+
+lmc_open_file(File, List) :-
+                read_file_to_codes(File, X, []),
+                string_codes(Y, X),
+                string_lower(Y, String),
+                split_string(String, "\n", "", List).
+
+lmc_remove_initial_spaces([],[]).
+lmc_remove_initial_spaces([H|T], [H2|Z]) :-
+                      split_string(H, "", "\s\t\n", List),
+                      nth0(0, List, H2, _),
+                      lmc_remove_initial_spaces(T, Z).
+
+lmc_remove_comment_line([],[]).
+lmc_remove_comment_line([H|T], Z) :-
+                        sub_string(H, Before, _, _, "//"),
+                        Before = 0, !,
+                        lmc_remove_comment_line(T, Z).
+lmc_remove_comment_line([H|T], [H|Z]) :-
+                        lmc_remove_comment_line(T, Z),
+                        !.
+
+lmc_remove_empty_line([],[]).
+lmc_remove_empty_line([H|T], Z) :-
+                      H="",
+                      lmc_remove_empty_line(T,Z).
+lmc_remove_empty_line([H|T], [H|Z]) :-
+                      lmc_remove_empty_line(T,Z),
+                      !.
+
+lmc_format_instruction_list(List, NewList) :-
+                        lmc_remove_initial_spaces(List, X),
+                        lmc_remove_comment_line(X, Y),
+                        lmc_remove_empty_line(Y, NewList).
+
+lmc_parse_labels([],[],[]).
+lmc_parse_labels([H|T], [Y|Z], [0|Xs]) :-
+                      split_string(H, "//", " ", [X|_]),
+                      split_string(X, " ", " ", Y),
+                      nth0(0, Y, First, _),
+                      member(First, ["add", "sub", "sta", "lda", "bra", "brz",
+                                  "brp", "inp", "out", "hlt", "halt"]), !,
+                      lmc_parse_labels(T, Z, Xs).
+
+lmc_parse_labels([H|T], [Rest|Z], [Label|Xs]) :-
+                          split_string(H, "//", " ", [X|_]),
+                          split_string(X, " ", " ", Y),
+                          nth0(0, Y, Label, Rest),
+                          lmc_parse_labels(T, Z, Xs).
+
+lmc_parse_instructions([],[],_).
+lmc_parse_instructions([[Op, Ind]|T], [MemCode| Z], Labels) :-
+                        Op = "add",
+                        OpN = 100,
+                        number_string(IndN,Ind), !,
+                        IndN < 100,
+                        MemCode is OpN + IndN,
+                        lmc_parse_instructions(T, Z, Labels).
+lmc_parse_instructions([[Op, Ind]|T], [MemCode | Z], Labels) :-
+                        Op = "add",
+                        OpN = 100,
+                        nth0(IndN, Labels, Ind, _),
+                        MemCode is OpN + IndN, !,
+                        lmc_parse_instructions(T, Z, Labels).
+
+lmc_parse_instructions([[Op, Ind]|T], [MemCode| Z], Labels) :-
+                        Op = "sub",
+                        OpN = 200,
+                        number_string(IndN,Ind), !,
+                        IndN < 100,
+                        MemCode is OpN + IndN,
+                        lmc_parse_instructions(T, Z, Labels).
+lmc_parse_instructions([[Op, Ind]|T], [MemCode | Z], Labels) :-
+                        Op = "sub",
+                        OpN = 200,
+                        nth0(IndN, Labels, Ind, _),
+                        MemCode is OpN + IndN, !,
+                        lmc_parse_instructions(T, Z, Labels).
+
+lmc_parse_instructions([[Op, Ind]|T], [MemCode| Z], Labels) :-
+                        Op = "sta",
+                        OpN = 300,
+                        number_string(IndN,Ind), !,
+                        IndN < 100,
+                        MemCode is OpN + IndN,
+                        lmc_parse_instructions(T, Z, Labels).
+lmc_parse_instructions([[Op, Ind]|T], [MemCode | Z], Labels) :-
+                        Op = "sta",
+                        OpN = 300,
+                        nth0(IndN, Labels, Ind, _),
+                        MemCode is OpN + IndN, !,
+                        lmc_parse_instructions(T, Z, Labels).
+
+lmc_parse_instructions([[Op, Ind]|T], [MemCode| Z], Labels) :-
+                        Op = "lda",
+                        OpN = 500,
+                        number_string(IndN,Ind), !,
+                        IndN < 100,
+                        MemCode is OpN + IndN,
+                        lmc_parse_instructions(T, Z, Labels).
+lmc_parse_instructions([[Op, Ind]|T], [MemCode | Z], Labels) :-
+                        Op = "lda",
+                        OpN = 500,
+                        nth0(IndN, Labels, Ind, _),
+                        MemCode is OpN + IndN, !,
+                        lmc_parse_instructions(T, Z, Labels).
+
+lmc_parse_instructions([[Op, Ind]|T], [MemCode| Z], Labels) :-
+                        Op = "bra",
+                        OpN = 600,
+                        number_string(IndN,Ind), !,
+                        IndN < 100,
+                        MemCode is OpN + IndN,
+                        lmc_parse_instructions(T, Z, Labels).
+lmc_parse_instructions([[Op, Ind]|T], [MemCode | Z], Labels) :-
+                        Op = "bra",
+                        OpN = 600,
+                        nth0(IndN, Labels, Ind, _),
+                        MemCode is OpN + IndN, !,
+                        lmc_parse_instructions(T, Z, Labels).
+
+lmc_parse_instructions([[Op, Ind]|T], [MemCode| Z], Labels) :-
+                        Op = "brz",
+                        OpN = 700,
+                        number_string(IndN,Ind), !,
+                        IndN < 100,
+                        MemCode is OpN + IndN,
+                        lmc_parse_instructions(T, Z, Labels).
+lmc_parse_instructions([[Op, Ind]|T], [MemCode | Z], Labels) :-
+                        Op = "brz",
+                        OpN = 700,
+                        nth0(IndN, Labels, Ind, _),
+                        MemCode is OpN + IndN, !,
+                        lmc_parse_instructions(T, Z, Labels).
+
+lmc_parse_instructions([[Op, Ind]|T], [MemCode| Z], Labels) :-
+                        Op = "brp",
+                        OpN = 800,
+                        number_string(IndN,Ind), !,
+                        IndN < 100,
+                        MemCode is OpN + IndN,
+                        lmc_parse_instructions(T, Z, Labels).
+lmc_parse_instructions([[Op, Ind]|T], [MemCode | Z], Labels) :-
+                        Op = "brp",
+                        OpN = 800,
+                        nth0(IndN, Labels, Ind, _),
+                        MemCode is OpN + IndN, !,
+                        lmc_parse_instructions(T, Z, Labels).
+
+lmc_parse_instructions([[Op]|T], [MemCode| Z], Labels) :-
+                        Op = "inp",
+                        MemCode = 901, !,
+                        lmc_parse_instructions(T, Z, Labels).
+
+lmc_parse_instructions([[Op]|T], [MemCode| Z], Labels) :-
+                        Op = "out",
+                        MemCode = 902, !,
+                        lmc_parse_instructions(T, Z, Labels).
+
+lmc_parse_instructions([[Op]|T], [MemCode| Z], Labels) :-
+                        Op = "hlt",
+                        MemCode = 000, !,
+                        lmc_parse_instructions(T, Z, Labels).
+
+lmc_parse_instructions([[Op, Ind]|T], [IndN| Z], Labels) :-
+                        Op = "dat",
+                        number_string(IndN,Ind), !,
+                        IndN < 100,
+                        lmc_parse_instructions(T, Z, Labels).
+lmc_parse_instructions([[Op, Ind]|T], [IndN | Z], Labels) :-
+                        Op = "dat",
+                        nth0(IndN, Labels, Ind, _), !,
+                        lmc_parse_instructions(T, Z, Labels).
+lmc_parse_instructions([[Op]|T], [0| Z], Labels) :-
+                        Op = "dat",!,
+                        lmc_parse_instructions(T, Z, Labels).
+
+lmc_load(File, Mem) :-
+              lmc_open_file(File, List),
+              lmc_format_instruction_list(List, FormattedList),
+              lmc_parse_labels(FormattedList, InstList, Labels),
+              lmc_parse_instructions(InstList, Mem, Labels).
+
+lmc_run(File, In, Out) :-
+							lmc_load(File, Mem),
+							execution_loop(state(0,0,Mem,In,[],noflag), Out).
