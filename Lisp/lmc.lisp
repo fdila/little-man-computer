2aa8474c8e4e874df3736a7f0f18701706d9591c
fdila <federicadilauro1998@gmail.com>
2018-12-21 09:40:07 +0100

    Aggiunte add, sub, store

--- Lisp/lmc.lisp
+++ Lisp/lmc.lisp
@@ -1,11 +1,3 @@
-;(defun state (&key acc pc)
-;  (list 'state acc pc))
-;(defun h-state (&key acc pc)
-;  (list 'h-state acc pc))
-;
-;  (cond ((eql (nth 0 state) 'state)
-;    (state :acc (+ (nth 1 state) (nth 2 state)) :pc (+ (nth 2 state) 1)))))
-
 (defun one-instruction (state)
   (let
     ((acc (nth 2 state))
@@ -14,4 +6,57 @@
      (in (nth 8 state))
      (out (nth 10 state))
      (flag (nth 12 state)))
-  (+ acc pc)))
+    (let
+      ((inst (nth pc mem))
+       (new-pc (mod (+ pc 1) 100)))
+      (cond
+        ;; ADD
+        ((and (> inst 99) (< inst 200))
+         (let ((sum (+ acc (nth (- inst 100) mem))))
+           (if (< sum 1000)
+             (list 'state
+                   :acc sum
+                   :pc new-pc
+                   :mem mem
+                   :in in
+                   :out out
+                   :flag 'noflag)
+             (list 'state
+                   :acc (mod sum 1000)
+                   :pc new-pc
+                   :mem mem
+                   :in in
+                   :out out
+                   :flag 'flag))))
+        ;; SUB
+        ((and (> inst 199) (< inst 300))
+         (let ((sub (- acc (nth (- inst 200) mem))))
+           (if (> sub 0)
+             (list 'state
+                   :acc sub
+                   :pc new-pc
+                   :mem mem
+                   :in in
+                   :out out
+                   :flag 'noflag)
+             (list 'state
+                   :acc (mod sub 1000)
+                   :pc new-pc
+                   :mem mem
+                   :in in
+                   :out out
+                   :flag 'flag))))
+        ;; STORE
+        ((and (> inst 299) (< inst 400))
+         (let ((addr (- inst 300)))
+           (setf (nth addr mem) acc)
+           (list 'state
+                 :acc acc
+                 :pc new-pc
+                 :mem mem
+                 :in in
+                 :out out
+                 :flag flag)))
+        ))
+    )
+  )
